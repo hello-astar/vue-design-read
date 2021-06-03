33e0f7cda555aa55b20197374464097f9b9a29d5
astar <2320390964@qq.com>
2021-06-03 10:29:15 +0800

    feat: 能算出所有解的求最长递增子序列算法

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 // 计算最长递增子序列
-export function lis (source) {
+export function lisMine (source) {
   let m = source.length
   let f = [...Array(m + 1).keys()].map(() => new Array(m + 1).fill(0))
   let application = [...Array(m + 1).keys()].map(() => new Array(m + 1).fill(-Infinity)) // 记录最后一位
@@ -41,4 +41,47 @@ export function lis (source) {
     i--
   }
   return res
+}
+
+export function lis (seq) {
+  const valueToMax = {}
+  let len = seq.length
+  for (let i = 0; i < len; i++) {
+    valueToMax[seq[i]] = 1
+  }
+
+  let i = len - 1
+  let last = seq[i]
+  let prev = seq[i - 1]
+  while (typeof prev !== 'undefined') {
+    let j = i
+    while (j < len) {
+      last = seq[j]
+      if (prev < last) {
+        const currentMax = valueToMax[last] + 1
+        valueToMax[prev] =
+          valueToMax[prev] !== 1
+            ? valueToMax[prev] > currentMax
+              ? valueToMax[prev]
+              : currentMax
+            : currentMax
+      }
+      j++
+    }
+    i--
+    last = seq[i]
+    prev = seq[i - 1]
+  }
+
+  const lis = []
+  i = 1
+  while (--len >= 0) {
+    const n = seq[len]
+    if (valueToMax[n] === i) {
+      i++
+      lis.unshift(len)
+    }
+  }
+
+  return lis
 }
